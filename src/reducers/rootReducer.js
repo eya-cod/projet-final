bf308805ec74a433076b20890fb38ff0e63191e3
eya-cod <74149792+eya-cod@users.noreply.github.com>
2021-03-16 16:17:18 +0100

    Delete src directory

--- src/reducers/rootReducer.js
+++
@@ -1,7 +0,0 @@
-import { combineReducers } from 'redux';
-import authReducer from './authReducer';
-import rdvReducer from './rdvReducer';
-import biensReducer from './biensReducer';
-import usersReducer from './usersReducer';
-
-export default combineReducers({ authReducer,rdvReducer,biensReducer,usersReducer });
